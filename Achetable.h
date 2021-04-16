3a0cdd301afb8e7b0c9cf4bbab19990189111c33
DimitriPaumard <>
2021-04-16 14:49:59 +0000

    ajout de Constuctible.h

---
+++ Achetable.h
@@ -0,0 +1,18 @@
+#ifndef ACHETABLE_H
+#define ACHETABLE_H
+
+
+
+class bonhomme
+{
+public: //variables accessibles par l'utilisateur
+ 
+
+
+private: //variables accessibles seulement par les éléments de la classe
+
+
+
+};
+
+#endif // BONHOMME_H
