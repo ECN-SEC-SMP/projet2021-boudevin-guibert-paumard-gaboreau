2e6a6e317740a64ef2381dde124849a32051e849
ThoGaboreau <>
2021-04-19 09:05:26 +0000

    ajout get id et mise à jour partie.h

--- Prison.cpp
+++ Prison.cpp
@@ -9,7 +9,7 @@ int get_loyer(){return (0);};
 
 void Prison::action(Joueur *j){
 
-  // le joueur passe en prison
-  j.set_injail();
+  // le joueur passe en prison (prison = 1)
+  j->set_jail();
 
 };
