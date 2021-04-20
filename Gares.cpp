b397fd6e98e53bf1c866452d045f5e660f570909
LouisGuibert <>
2021-04-20 17:36:01 +0000

    modification affiche case Gares.cpp

--- Gares.cpp
+++ Gares.cpp
@@ -1,5 +1,43 @@
 #include "Achetable.h"
 #include "Gares.h"
+#include <iostream>     // std::cout
+#include <algorithm>    // std::replace
+#include <vector>       // std::vector
+#include <iterator>
+
+void Gares::afficher_case() {
+  // création du vecteur
+  vector<string> vs;
+  
+  // on ajoute le nom de la case
+  vs.push_back(this->nom);
+
+  //on ajoute le coût
+  vs.push_back ("(coût : "+ to_string( this->prix_const)+")");
+  
+ // test si pas de propriétaire
+ if(this->proprietaire == nullptr){
+
+  vs.push_back("- sans propriétaire");
+
+ }
+ // si il y a un propriétaire
+ else{
+  
+  vs.push_back("propriétaire :");
+  vs.push_back(this->proprietaire->nom);
+
+  //affichage loyer
+  vs.push_back("loyer =");
+  vs.push_back(to_string(this->loyer));
+ }
+
+  copy(vs.begin(),vs.end(),ostream_iterator<string>(cout," "));
+  cout << endl;
+
+};
+
+
 
 int Gares::get_id_gare() const
 {
@@ -24,13 +62,6 @@ Gares::Gares(int idgare, int prix_gare, string nom_const)
     this->nom = nom_const;
 }
 
-void Gares::afficher_case()
-{
-    if(this->proprietaire == nullptr)
-        cout<<"cette case ne possède pas de proprio";
-    else 
-        cout<<"le propiétaire de cette case est à " <<this->proprietaire->get_id()<<endl;
-}
 int Gares::get_loyer() //retourne le loyer que doit payer un joueur en fonction du nombre de gare possédé
 {
     return((this->proprietaire->get_nb_gares())*2500);
