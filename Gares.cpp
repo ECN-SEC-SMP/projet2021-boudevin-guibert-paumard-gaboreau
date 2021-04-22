a3668ea131135654689e66e568de3a72e612fa4d
user <user@example.com>
2021-04-22 16:31:50 +0200

    Correction bug Const

--- Gares.cpp
+++ Gares.cpp
@@ -1,29 +1,10 @@
-#include "Achetable.h"
-#include "Gares.h"
-#include <iostream>     // std::cout
-#include <algorithm>    // std::replace
-#include <vector>       // std::vector
-#include <iterator>
+#include <iostream> 
+#include <stdio.h>  
+using namespace std;
 
-int Gares::get_id_gare() const //renvoie ne numéro d'identifiant de la gare
-{
-    return this->id_gare;
-}
-//todo : modifier pour qu'action ne serve qu'à payer le loyer et acheter des maisons/hotels
-void Gares::action(Joueur *cible) 
-{
-    if(proprietaire == nullptr) //si la case n'a pas de propriétaire
-    {
-        if(cible ->get_fortune() >= this->prix) // si le joueur à assez de sous
-        {
-            this -> proprietaire = cible;
-        }
-    }
-    else if(proprietaire != nullptr)
-    {
-        cible -> paiement(this ->get_loyer(),this ->  proprietaire);
-    }
-}
+#include "Achetable.h"
+#include "Joueur.h"
+#include "Gares.h"
 
 Gares::Gares(int idgare, int prix_gare, string nom_const)
 {
@@ -33,10 +14,41 @@ Gares::Gares(int idgare, int prix_gare, string nom_const)
     this->proprietaire = nullptr;
 }
 
+void Gares::acheter(Joueur *cible) 
+{
+    cible->add_fortune(-this->get_prix());
+    this-> proprietaire = cible;
+}
+
+//todo : modifier pour qu'action ne serve qu'à payer le loyer et acheter des maisons/hotels
+void Gares::action(Joueur *cible) 
+{
+    if(this->proprietaire == nullptr) //si la case n'a pas de propriétaire
+    {
+        if(cible ->get_fortune() >= this->prix) // si le joueur à assez de sous
+        {
+            this->proprietaire = cible;
+        }
+    }
+    else if(this->proprietaire != nullptr)
+    {
+        cible->paiement(this->get_loyer(),this->proprietaire);
+    }
+}
+
+bool Gares::is_available() const //renvoie 1 si il y a un proprio, 0 sinon
+{
+    return this->get_proprietaire() == nullptr;
+}
+
 int Gares::get_loyer() const //retourne le loyer que doit payer un joueur en fonction du nombre de gare possédé
 {
     return((this->proprietaire->get_nb_gares())*(this->loyer));
 }
+
+string Gares::get_nom() const{
+    return this->nom;
+};
 
 Joueur *Gares::get_proprietaire() const
 {
@@ -48,21 +60,7 @@ int Gares::get_prix() const
     return this->prix;
 }
 
-void Gares::acheter(Joueur *cible) 
+int Gares::get_id_gare() const //renvoie ne numéro d'identifiant de la gare
 {
-    cible ->add_fortune(-1*(this ->get_prix()));
-    this-> proprietaire = cible;
+    return this->id_gare;
 }
-
-
-bool Gares::is_available() const //renvoie 1 si il y a un proprio, 0 sinon
-{
-    if(this ->get_proprietaire() == nullptr)
-    return 0;
-    else
-    return 1;
-}
-
-string Gares::get_nom() const{
-    return this->nom;
-};
