d8b918de36342514708650affe7dc1a5cdbda9a9
LouisGuibert <>
2021-04-25 17:35:39 +0000

    modification affichage mineur

--- Prison.h
+++ Prison.h
@@ -1,6 +1,10 @@
 #ifndef Prison_h 
 #define Prison_h 
 
+//!@file Chance.h
+//!@brief classe fille de de Case
+//!@author Louis
+
 #include "Case.h"
 
 class Prison : public Case
@@ -9,7 +13,9 @@ class Prison : public Case
   public :
 
     Prison();
-  
+    //! @brief la méthode action permet ici de passer le paramètre Jail du joueur à True
+    //! @param joueur le paramètre cible est un pointeur sur un objet joueur
+    //! @return void
     virtual void action(Joueur *j) override ; //Chance hérite de sa propre classe action
 
     std::ostream & do_print(std::ostream & c) const override{
